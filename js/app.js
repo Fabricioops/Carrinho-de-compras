659ecce9d10159fa261c1ff9c039ce346564b46e
Fabricio Santana <fabricioztxsantana@gmail.com>
2025-01-25 16:13:21 -0300

    ajustes finais no total geral

--- js/app.js
+++ js/app.js
@@ -1,25 +1,35 @@
 let totalGeral = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').innerHTML = '';
+
 
 function adicionar() {
-    //recuperar valores nome do produto, quantidade e valor
-    let produto = document.getElementById('produto').value;
-    let nomeProduto = produto.split('-')[0];
-    let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade').value; 
-    
-    //calcular o preço, o nosso subtotal
+    // Recuperar valores - nome do produto, quantidade e valor
+    let Produto = document.getElementById('produto').value;
+    let nomeProduto = Produto.split('-')[0];
+    let valorUnitario = Produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    console.log(nomeProduto);
+    console.log(valorUnitario);
+    console.log(quantidade);
+
+    // Calcular o preço, o subtotal
     let preco = quantidade * valorUnitario;
+    console.log(preco);
+
+    // Adicionar no carrinho
     let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML += `<section class="carrinho_produtos_produto">
+        <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+    </section>`;
 
-    //adicionar no carrinho
-    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
-  </section>`;
-
-    //atualizar o valor total
+    // Atualizar o total da compra
     totalGeral = totalGeral + preco;
-    let campoTotal = document.getElementById('valor-total'); 
-    campoTotal.textContent = R$ ${totalGeral}``;
+
+    let totalizando = document.getElementById('valor-total');
+    totalizando.innerHTML = `<p class="carrinho__total">
+        Total: <span class="texto-azul" id="valor-total">R$${totalGeral}</span>
+    </p>`;
 }
 
 function limpar() {
